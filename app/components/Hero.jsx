f7f120a597467e6ca5b37b9514e14a459157ff9b
coreybeautyman <corey.beautyman@gmail.com>
2024-10-01 18:18:46 +0100

    adding image loading

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,6 +5,7 @@ import profileImg from '../../public/Corey-headshot.jpg'
 import { PiLinkedinLogoBold } from 'react-icons/pi'
 import { FiGithub } from 'react-icons/fi'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 function Hero() {
   const isLarge = () => {
@@ -12,6 +13,12 @@ function Hero() {
       return window.innerWidth >= 1024 ? true : false
     }
     return false
+  }
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
+
+  const handleIsImageLoaded = () => {
+    setIsImageLoaded(true)
   }
 
   return (
@@ -28,60 +35,66 @@ function Hero() {
         <Image
           src={profileImg}
           alt="profile image"
-          className="max-w-72 rounded-full border-4 border-green-600 object-cover transition-all duration-700"
+          className={
+            isImageLoaded &&
+            'max-w-72 rounded-full border-4 border-green-600 object-cover transition-all duration-700'
+          }
+          onLoadingComplete={handleIsImageLoaded}
         />
       </motion.div>
-      <div className="flex flex-col justify-center">
-        <motion.h1
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: isLarge() ? 0.7 : 1 }}
-          className="mb-7 text-center text-5xl font-extrabold lg:text-left lg:text-6xl"
-        >
-          Junior Full-stack <br />
-          Developer
-        </motion.h1>
-        <motion.p
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 1 }}
-          className="mb-11 max-w-2xl text-center text-lg lg:w-10/12 lg:text-left"
-        >
-          Hi, I'm Corey Beautyman, I'm a junior full stack developer with a
-          strong foundation in modern web technologies. I build scalable,
-          user-friendly applications that bring creative ideas to life, from
-          front-end design to back-end functionality.
-        </motion.p>
+      {isImageLoaded && (
+        <div className="flex flex-col justify-center">
+          <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: isLarge() ? 0.7 : 1 }}
+            className="mb-7 text-center text-5xl font-extrabold lg:text-left lg:text-6xl"
+          >
+            Junior Full-stack <br />
+            Developer
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="mb-11 max-w-2xl text-center text-lg lg:w-10/12 lg:text-left"
+          >
+            Hi, I'm Corey Beautyman, I'm a junior full stack developer with a
+            strong foundation in modern web technologies. I build scalable,
+            user-friendly applications that bring creative ideas to life, from
+            front-end design to back-end functionality.
+          </motion.p>
 
-        <div className="flex items-center justify-center space-x-4 text-4xl lg:justify-start">
-          <motion.span
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.3 }}
-          >
-            <a
-              href={'https://www.linkedin.com/in/corey-beautyman-5639bb12b/'}
-              target="_blank"
-              rel="noopener noreferrer"
+          <div className="flex items-center justify-center space-x-4 text-4xl lg:justify-start">
+            <motion.span
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 1.3 }}
             >
-              <PiLinkedinLogoBold className="cursor-pointer duration-300 ease-in-out hover:scale-110 hover:text-green-600" />
-            </a>
-          </motion.span>
-          <motion.span
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.4 }}
-          >
-            <a
-              href={'https://github.com/coreybeautyman'}
-              target="_blank"
-              rel="noopener noreferrer"
+              <a
+                href={'https://www.linkedin.com/in/corey-beautyman-5639bb12b/'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <PiLinkedinLogoBold className="cursor-pointer duration-300 ease-in-out hover:scale-110 hover:text-green-600" />
+              </a>
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 1.4 }}
             >
-              <FiGithub className="cursor-pointer duration-300 ease-in-out hover:scale-110 hover:text-green-600" />
-            </a>
-          </motion.span>
+              <a
+                href={'https://github.com/coreybeautyman'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiGithub className="cursor-pointer duration-300 ease-in-out hover:scale-110 hover:text-green-600" />
+              </a>
+            </motion.span>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
